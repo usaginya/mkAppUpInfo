d9c6b1d0365dce30add660eb5853f89823a738a8
YIU <usaginya@vip.qq.com>
2018-01-05 22:40:41 +0800

    Add files via upload

---
+++ MoeLoader-Delta/donate/assets/js/functions.js
@@ -0,0 +1,15 @@
+$(window).load(function() {
+    
+    $('.tag').each(function(i) {
+    	setTimeout(function() {
+    		$('.tag:eq('+i+')').css({ display: 'block', opacity: 0 }).stop().animate({ opacity: 1 }, 'easeInOutExpo'); 
+    	}, 32 * (i + 1))
+    });
+
+	$('.tag').hover(function() {
+		$(this).stop().animate({ paddingRight: ($('.tag_count', this).outerWidth() - 5), opacity: 1 }, 'easeInOutExpo');
+	}, function() {
+		$(this).stop().animate({ paddingRight: 5 }, 'easeInOutExpo');
+	});
+
+});
