d819323d68521742ddd52c901f8366fbbe7274f2
YIU <usaginya@vip.qq.com>
2018-02-20 00:48:46 +0800

    Update moe.moekai.gmgardtransparentpost.js

--- monkeyjs/moe.moekai.gmgardtransparentpost.js
+++ monkeyjs/moe.moekai.gmgardtransparentpost.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         绅士之庭透明文章
 // @namespace    moe.moekai.gmgardtransparentpost
-// @version      0.2
+// @version      0.3
 // @description  让绅士之庭的文章底色透明
 // @author       YIU
 // @match        http*://gmgard.com/*
@@ -16,14 +16,32 @@
 	$("#body").css("background-color","#ffffffaa");
 	$(".categories").css("background","#ffffff99");
 	$("#blog").css("background-color","#fff0");
-	$(".bubble").css("background","#ffffff90");
+
 	$(".home-list").css("background-color","#f7f7de50");
 	$("#rankContent").css("background-color","#daebf470");
 	$(".author-info").css("background","#f3f3f380");
 	$(".author-sign").css("background","#ffffff80");
 	$(".spoiler-content").css("background","none repeat scroll 0 0 #f5f5f590");
 	$(".spoiler-content span[style*='background-color']").css("background-color","#000000cc");
-	$(".bubble").append("<style>.bubble::after{border-width:10px 15px 10px 0;border-color:transparent #ffffffe6;}</style>");
+
 	$("#rankContent").append("<style>.rankflag::after{background:linear-gradient(150deg,rgba(218,235,244,.4) 0,rgba(218,235,244,.4) 50%,transparent 51%,transparent 100%)}</style>");
 	$("#rankContent").append("<style>.rankflag::before{background:linear-gradient(210deg,rgba(218,235,244,.4) 0,rgba(218,235,244,.4) 50%,transparent 51%,transparent 100%)}</style>");
+
+	$(window).scroll(function(){
+		if(getScrollPercent() == 60)
+		{
+			$(".bubble").css("background","#ffffff90");
+			$(".bubble").append("<style>.bubble::after{border-width:10px 15px 10px 0;border-color:transparent #ffffffe6;}</style>");
+		}
+	});
+
+	function getScrollPercent()
+	{
+		var scrollTo = $(window).scrollTop(),
+			docHeight = $(document).height(),
+			windowHeight = $(window).height();
+		scrollPercent = (scrollTo / (docHeight-windowHeight)) * 100;
+		scrollPercent = scrollPercent.toFixed(1);
+		return scrollPercent;
+	}
 })();
