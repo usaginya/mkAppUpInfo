e996e32c646c5ef28d37508d6b61a2f7805b8ea5
YIU <usaginya@vip.qq.com>
2018-03-06 20:52:38 +0800

    Update to version 1.8
    fix rank tab style
    add new style

--- monkeyjs/moe.moekai.gmgardtransparentpost.js
+++ monkeyjs/moe.moekai.gmgardtransparentpost.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         绅士之庭透明文章
 // @namespace    moe.moekai.gmgardtransparentpost
-// @version      1.7
+// @version      1.8
 // @description  让绅士之庭的文章底色透明
 // @author       YIU
 // @match        http*://gmgard.com/*
@@ -19,6 +19,7 @@
 	$("#blog .uimg").css("background","#fff9");
 	$(".well").css("background-color","#f5f5f5b8");
 	$("input.tagsearch").css("background-color","#f5f5f599");
+	$("span[style='background-color:#000000']").css("background","#000b");
 	$("hr").css({"border-top-color":"#eee8","border-bottom-color":"#eee8"});
 
 	$(".reply-section").css("background","#efeeef90");
@@ -27,15 +28,18 @@
 	$("#rankContent").css("background-color","#daebf470");
 	$(".author-info").css("background","#f3f3f380");
 	$(".author-sign").css("background","#ffffff80");
-	$(".nav-tabs .active a").css("background-color","#fffa");
 	$(".spoiler-content").css("background","none repeat scroll 0 0 #f5f5f590");
 	$(".spoiler-content span[style*='background-color']").css("background-color","#000000cc");
-	$("#main:contains('DailyRankings')").css("background","linear-gradient(135deg,#cebe29a6 0,#9b1f50a8 33%,#2989d8a8 71%,#89b4ffa8 91%)");
+	$("#main:contains('DailyRankings')").css("background","linear-gradient(135deg,#cebe29a6 0,#9b1f50a8 33%,#2989d8a8 71%,#89b4ffa8 91%);");
 	$(".navbar-inner").css({"background-color":"#fff0","background-image":"linear-gradient(to bottom,#ffffffa0,#f2f2f280)"});
+	$(".btn-info").css({"background":"transparent linear-gradient(to bottom,#5bc0dea0,#2f96b480)"});
 
-	var strs = "<style>.rankflag::after{background:linear-gradient(150deg,rgba(218,235,244,.4) 0,rgba(218,235,244,.4) 50%,transparent 51%,transparent 100%)}";
-	strs += ".rankflag::before{background:linear-gradient(210deg,rgba(218,235,244,.4) 0,rgba(218,235,244,.4) 50%,transparent 51%,transparent 100%)}</style>";
-	$("#rankContent").append(strs);
+	var strs = "<style>";
+	strs += ".rankflag::after{background:linear-gradient(150deg,rgba(218,235,244,.4) 0,rgba(218,235,244,.4) 50%,transparent 51%,transparent 100%)}";
+	strs += ".rankflag::before{background:linear-gradient(210deg,rgba(218,235,244,.4) 0,rgba(218,235,244,.4) 50%,transparent 51%,transparent 100%)}";
+	strs += ".nav-tabs .active a,.nav-tabs .active a:hover,.nav-tabs .active a:focus{background-color:#fffa}";
+	strs += "</style>";
+	$("head").append(strs);
 
 	//--- footer ---
 	$("footer").css("background-color","#e2e2e2e0");
@@ -78,13 +82,15 @@
 			$("#tag-input").css("background-color","#fffc");
 			$(".tag-info").css("background","#f3f3f3a0");
 		}
-		$(".tm-tag-info").css("background-color","#c5eefaa0");
+		$(".tm-tag-info").css({"color":"#4594b5","background-color":"#c5eefaa0"});
+		$(".tm-tag").not(".tm-tag-info").css({"color":"#368000","background-color":"#b1ff6b80"});
 	});
 
 	//---- comment edit box ----
 	$("#AddReplyForm").bind("DOMSubtreeModified",function(){
 		var bg_cke_inner = $(".cke_inner").css("background");
 		if(bg_cke_inner && bg_cke_inner.indexOf("0.")<1){
+			$(".cke_toolgroup").css("background","transparent");
 			$(".cke_top,.cke_bottom").css({"background":"transparent linear-gradient(rgba(229,245,250,.3), rgba(1,158,213,.3))"});
 			$(".cke_inner").css({"background":"transparent linear-gradient(to right, rgba(255,255,255,.5),rgba(255,255,255,.5),rgba(255,255,255,.5),rgba(255,255,255,1),rgba(255,255,255,1))"});
 		}
