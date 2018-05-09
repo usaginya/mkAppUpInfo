c02456250ce152edb86d39858cdddf1e6d40ba75
YIU <degdod@qq.com>
2018-05-10 03:04:28 +0800

    up

--- docs/medicinedance/js/functions.js
+++ docs/medicinedance/js/functions.js
@@ -1,9 +1,11 @@
-window.onload=function(){
-	v = document.getElementById("musics");
-	v.oncanplay=eve();
+var ev = {
+ 	a:function(){
+		$("#himg").css('display','block');
+		$("body").removeClass("whilebody");
+	}
 }
 
-function eve(){
-	$("#himg").css('display','block');
-	$("body").removeClass("whilebody");
+window.onload=function(){
+	v = document.getElementById("musics");
+	v.oncanplay=ev.a();
 }
