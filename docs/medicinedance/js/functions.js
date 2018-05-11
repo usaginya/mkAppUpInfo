e60bbd230a6d5ace2a57cac604dc911b280e322c
YIU <degdod@qq.com>
2018-05-11 20:09:18 +0800

    update medicine
    add mobile support

--- docs/medicinedance/js/functions.js
+++ docs/medicinedance/js/functions.js
@@ -1,13 +1,30 @@
 var ev = {
- 	a:function(){
+	a:function(){
 		$("#himg").css('display','block');
 		$("body").removeAttr("class");
 	}
 }
 
+function audioAutoPlay() {
+	var audio = document.getElementById('musics');
+	audio.play();
+	document.addEventListener("WeixinJSBridgeReady", function () {
+		audio.play();
+	}, false);
+}
+
 window.onload=function(){
-	v = document.getElementById("musics");
+	var v = document.getElementById("musics");
 	v.oncanplay=ev.a();
 	lrclib.showLrc('lrcbox','lrc/ジェニーはご机嫌ななめ-やくしまるえつこ.lrc','YIU','https://github.com/usaginya');
 	lrclib.showLrc('lrcbox2','lrc/ジェニーはご机嫌ななめ-やくしまるえつこCN.lrc','YIU','https://github.com/usaginya');
 }
+
+//--兼容手机--
+document.addEventListener('DOMContentLoaded', function () {
+	audioAutoPlay();
+});
+//--监听触摸 进行播放
+document.addEventListener('touchstart', function () {
+	audioAutoPlay();
+});
