5bdb04fb9076b8b87aee5773656391d84f4b64e9
YIU <usaginya@vip.qq.com>
2018-09-14 03:01:07 +0800

    Update moe.moekai.gmgardtransparentpost.user.js
    Add new transparent background

--- monkeyjs/moe.moekai.gmgardtransparentpost.user.js
+++ monkeyjs/moe.moekai.gmgardtransparentpost.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         绅士之庭透明文章
 // @namespace    moe.moekai.gmgardtransparentpost
-// @version      2.0
+// @version      2.1
 // @description  让绅士之庭的文章底色透明
 // @author       YIU
 // @match        http*://gmgard.com/*
@@ -20,6 +20,7 @@
 	$(".well").css("background-color","#f5f5f5b8");
 	$("input.tagsearch").css("background-color","#f5f5f599");
 	$("span[style='background-color:#000000']").css("background","#000000a5");
+	$("span[style='background-color:#ffffff']").css("background","#fffb");
 	$("hr").css({"border-top-color":"#eee8","border-bottom-color":"#eee8"});
 
 	$(".reply-section").css("background","#efeeef90");
@@ -36,7 +37,16 @@
 	$("#multiview table").css({"background-color":"#fff6"});
 
 	var strs = "<style>";
+	strs += ".label{background-color:#9999}";
 	strs += "pre{background-color:#f5f5f599}";
+	strs += ".btn-warning{background-image: linear-gradient(to bottom,#fbb45099,#f8940699)}";
+	strs += ".label-info[href],.badge-info[href]{background-color:#2d6987b5}";
+	strs += ".label-important,.badge-important{background-color:#b94a48d6}";
+	strs += ".label-info,.badge-info{background-color:#3a87adcc}";
+	strs += ".label-inverse, .badge-inverse{background-color:#0008}";
+	strs += ".btn-primary{background-image:linear-gradient(to bottom,transparent,transparent);background-color:#006dcc99}";
+	strs += ".btn-primary:hover,.btn-primary:focus,.btn-primary:active,.btn-primary.active,.btn-primary.disabled,.btn-primary[disabled]{background-color:#04c9}";
+	strs += ".nav-pills>.active>a,.nav-pills>.active>a:hover,.nav-pills>.active>a:focus{background-color:#08ca}";
 	strs +=".user-bg h2,.user-bg .user-stats,.user-bg .user-numbers-div,.userinfo h3{background-color:transparent!important}";
 	strs += ".rankflag::after{background:linear-gradient(150deg,rgba(218,235,244,.4) 0,rgba(218,235,244,.4) 50%,transparent 51%,transparent 100%)}";
 	strs += ".rankflag::before{background:linear-gradient(210deg,rgba(218,235,244,.4) 0,rgba(218,235,244,.4) 50%,transparent 51%,transparent 100%)}";
@@ -44,7 +54,9 @@
 	strs += ".feed-item{background-color:#fff9}";
 	strs += ".feed-footer{background:#f7f6f980}";
 	strs += ".tabcontent{background-color:#fff4}";
-	strs += "#msgmenu .active a{background-color:transparent}";
+	strs += ".tooltip-inner{background-color:#000b}";
+	strs += "#flinkdiv img{opacity:.5}";
+	strs += "#msgmenu .active a,.btn-warning{background-color:transparent}";
 	strs += ".msglist-item:hover{background-color:#c0c0c090}";
 	strs += ".dropdown-menu{background-color:#fffe}";
 	strs += ".nav-pills .open .dropdown-toggle{background-color:#999a}";
