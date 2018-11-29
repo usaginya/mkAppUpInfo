48f564b73cf1719f3cc29611435be047bfe950bd
YIU <degdod@qq.com>
2018-11-29 17:31:31 +0800

    Update MLD to version 7.0.6.3
    And donate page.

--- docs/moeloaderdelta/donate/assets/js/functions.js
+++ docs/moeloaderdelta/donate/assets/js/functions.js
@@ -1,16 +1,37 @@
 $(window).load(function() {
-    
-    $('.tag').each(function(i) {
-    	setTimeout(function() {
-    		$('.tag:eq('+i+')').css({ display: 'block', opacity: 0 }).stop().animate({ opacity: 1 }, 'easeInOutExpo'); 
-    	}, 32 * (i + 1))
-    });
+  const C_colors = {
+                link: '#007dd8',
+                no1: '#c0f',
+                no2: '#f00',
+                no3: '#fb0'
+              }
 
-	$('.tag').hover(function() {
-		$(this).stop().animate({ paddingRight: ($('.tag_count', this).outerWidth() - 5), opacity: 1 }, 'easeInOutExpo');
-	}, function() {
-		$(this).stop().animate({ paddingRight: 5 }, 'easeInOutExpo');
-	});
-	
+  $('#donatelist').hide();
 
+  var $tags = $('.tag');
+  $tags.sort(function(a,b){
+    var aV = parseFloat($(a).find('.tag_count').attr('title'));
+    var bV = parseFloat($(b).find('.tag_count').attr('title'));
+
+    if(aV < bV)			return 1;
+    else if(aV > bV)	return -1;
+    else				return 0;
+  });
+  $tags.detach().appendTo('#donatelist');
+
+  $('#donatelist').show();
+
+  $tags.each(function(i) {
+    setTimeout(function($tag) {
+      $tag.css({ display: 'block', opacity: 0 }).stop().animate({ opacity: 1 }, 'easeInOutExpo');
+      switch(i)
+      {
+        case 0: $tag.css({ color: C_colors.no1 }); break;
+        case 1: $tag.css({ color: C_colors.no2 }); break;
+        case 2: $tag.css({ color: C_colors.no3 }); break;
+        default:
+          $tag.not('[href=#]').css({ color: C_colors.link });
+      }
+    }, 32 * (i + 1), $(this));
+  });
 });
