25232d35b8e6b27213e30e27890eff94f4fabccf
YIU <degdod@qq.com>
2019-03-22 10:56:48 +0800

    update gmjs exhentai-tooltip
    ver 0.5
    Fix bug.

--- monkeyjs/moe.moekai.exhentai-tooltip.user.js
+++ monkeyjs/moe.moekai.exhentai-tooltip.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ExHentai列表标题悬浮提示
 // @namespace    moe.moekai.exhentai-tooltip
-// @version      0.4
+// @version      0.5
 // @description  给ExHentai缩略图列表标题添加悬浮提示来显示完整标题
 // @author       YIU
 // @icon         https://exhentai.org/favicon.ico
@@ -17,25 +17,24 @@
 
 (function() {
 
-	var headlabel = '<link rel="stylesheet" href="https://cdn.bootcss.com/jqueryui/1.11.0/jquery-ui.min.css">';
-	headlabel += '<style>.ui-tooltip,.arrow:after{background:#001b;border:1px solid #446}.ui-tooltip{padding:8px 10px;color:white;font:14px"Microsoft yahei",Sans-Serif;box-shadow:0 0 7px #446}</style>';
+	var headlabel = `<link rel="stylesheet" href="https://cdn.bootcss.com/jqueryui/1.11.0/jquery-ui.min.css">
+<style>
+.ui-tooltip,.arrow:after{background:#001a;border:1px solid #446}
+.ui-tooltip{padding:8px 10px;color:white;font-size:11pt;box-shadow:0 0 7px #446;max-width:250px;z-index:9}
+</style>`;
 
 	$('head').append(headlabel);
 
-	$('.itg').tooltip({
+	$('.gl3t').tooltip({
 		position:{
 			using:function(position){
+				position.top += 25;
+				position.left += 20;
 				$(this).css(position);
 				$("<div>").addClass("arrow").appendTo(this);
 			}
 		},
 		track:true
-	});
-
-	$(".id2 a").each(function(){
-		$(this).attr({
-			"title" : $(this).text()
-		});
 	});
 
 })();
