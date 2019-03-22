94701be9958453709ff56938f58d359ef155425d
YIU <degdod@qq.com>
2019-03-22 13:03:11 +0800

    Update moe.moekai.exhentai-tooltip.user.js
    Update info.
    Update jquery library.

--- monkeyjs/moe.moekai.exhentai-tooltip.user.js
+++ monkeyjs/moe.moekai.exhentai-tooltip.user.js
@@ -1,23 +1,27 @@
 // ==UserScript==
-// @name         ExHentai列表标题悬浮提示
-// @namespace    moe.moekai.exhentai-tooltip
-// @version      0.5
-// @description  给ExHentai缩略图列表标题添加悬浮提示来显示完整标题
+// @name                ExHentai thumbnail list title hover prompt
+// @name:zh-CN          ExHentai缩略图列表标题悬浮提示
+// @name:zh-TW          ExHentai縮略圖列表標題懸浮提示
+// @namespace           moe.moekai.exhentai-tooltip
+// @version             0.6
+// @description         Add a hover hint to the ExHentai thumbnail list title to display the full title
+// @description:zh-CN   给ExHentai缩略图列表标题添加悬浮提示来显示完整标题
+// @description:zh-TW   給ExHentai縮略圖列表標題添加懸浮提示來顯示完整標題
 // @author       YIU
 // @icon         https://exhentai.org/favicon.ico
 // @match        *://exhentai.org/
 // @match        *://exhentai.org/?*
 // @match        *://e-hentai.org/
 // @match        *://e-hentai.org/?*
-// @require      https://cdn.bootcss.com/jquery/2.2.0/jquery.min.js
-// @require      https://cdn.bootcss.com/jqueryui/1.10.4/jquery-ui.min.js
+// @require      https://code.jquery.com/jquery-2.1.1.min.js
+// @require      https://code.jquery.com/ui/1.11.0/jquery-ui.min.js
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
 
 (function() {
 
-	var headlabel = `<link rel="stylesheet" href="https://cdn.bootcss.com/jqueryui/1.11.0/jquery-ui.min.css">
+	var headlabel = `<link rel="stylesheet" href="https://code.jquery.com/ui/1.11.0/themes/smoothness/jquery-ui.css">
 <style>
 .ui-tooltip,.arrow:after{background:#001a;border:1px solid #446}
 .ui-tooltip{padding:8px 10px;color:white;font-size:11pt;box-shadow:0 0 7px #446;max-width:250px;z-index:9}
