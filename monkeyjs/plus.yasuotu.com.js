050e4beb27df4be40a37e0c24c40207d87ed1b44
YIU <degdod@qq.com>
2019-07-29 19:17:18 +0800

    Update plus.yasuotu.com.js
    v2

--- monkeyjs/plus.yasuotu.com.js
+++ monkeyjs/plus.yasuotu.com.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         压缩图-功能提升
 // @namespace    www.yasuotu.com
-// @version      190729.0.1
+// @version      190729.0.2
 // @description  免会员下载原图,提升图片上传数量/大小限制
 // @author       YIU
 // @match        https://www.yasuotu.com/*
@@ -36,7 +36,30 @@ unsafeWindow.Object.freeze = null;
 			if($('.user-cz font')[0]){
 				$('.user-cz font').html($('.user-cz font').html().replace('普通','脚本'));
 			}
+			BindSaveBtn();
 		},500);
 	});
+
+	function BindSaveBtn(){
+		if($('.filelist').length>0){
+			$('.filelist').on("DOMSubtreeModified",()=>{
+
+				if($('.btn-dowload').length>0){
+					$('.filelist').off("DOMSubtreeModified");
+
+					$('.filelist .btn-dowload').each((i,o)=>{
+
+						$(o).removeAttr('onclick');
+						$(o).attr('href', $(o).attr('url'));
+
+					});
+
+					BindSaveBtn();
+				}
+
+			});
+		}
+	}
+
 
 })();
