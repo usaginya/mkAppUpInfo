4a67ca5394fc226c738396043212720bd5cddb31
YIU <degdod@qq.com>
2019-09-11 15:31:00 +0800

    Add joke html+js

---
+++ docs/joke/js/joke.js
@@ -0,0 +1,7 @@
+jokes = ["在mrange庆典的聚会上，一位35岁的mvictim高举着牌子，上面写着“感谢()赐予我的快乐的童年”。</br>mman呵斥道，“你是在嘲讽mthing吗？mthing才实行了20年。”</br>“确切地说，这正是我感谢它的原因。”
+",]
+
+alert(2);
+$("#mgo").click(function(){
+	alert(1);
+})
