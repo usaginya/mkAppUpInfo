933f95aea487dc7bcee4e6b4c5a6b28882591555
YIU <usaginya@vip.qq.com>
2020-08-01 17:01:06 +0800

    Update mld info to version7.0.7.5

--- docs/moeloaderdelta/donate/assets/js/functions.js
+++ docs/moeloaderdelta/donate/assets/js/functions.js
@@ -1,38 +1,78 @@
 $(window).load(function() {
-  const C_colors = {
-                link: '#007dd8',
-                no1: '#c0f',
-                no2: '#f00',
-                no3: '#fb0'
-              }
+    DonateBtnBind();
 
-  $('#donatelist').hide();
+    const C_colors = {
+        link: '#007dd8',
+        no1: '#c0f',
+        no2: '#f00',
+        no3: '#fb0'
+    }
 
-  var $tags = $('.tag');
-  $tags.sort(function(a,b){
-    var aV = parseFloat($(a).find('.tag_count').attr('title'));
-    var bV = parseFloat($(b).find('.tag_count').attr('title'));
+    $('#donatelist').hide();
 
-    if($(a).find('.tag_name').text()=="[匿名]" || $(a).find('.tag_name').text()=="[匿名]") return 0;
-    else if(aV < bV)			return 1;
-    else if(aV > bV)	return -1;
-    else				return 0;
-  });
-  $tags.detach().appendTo('#donatelist');
+    var $tags = $('.tag');
+    $tags.sort(function(a, b) {
+        var aV = parseFloat($(a).find('.tag_count').attr('title'));
+        var bV = parseFloat($(b).find('.tag_count').attr('title'));
 
-  $('#donatelist').show();
+        if ($(a).find('.tag_name').text() == "[匿名]" || $(a).find('.tag_name').text() == "[匿名]")
+            return 0;
+        else if (aV < bV)
+            return 1;
+        else if (aV > bV)
+            return -1;
+        else
+            return 0;
+    });
+    $tags.detach().appendTo('#donatelist');
 
-  $tags.each(function(i) {
-    setTimeout(function($tag) {
-      $tag.css({ display: 'block', opacity: 0 }).stop().animate({ opacity: 1 }, 'easeInOutExpo');
-      switch(i)
-      {
-        case 0: $tag.css({ color: C_colors.no1 }); break;
-        case 1: $tag.css({ color: C_colors.no2 }); break;
-        case 2: $tag.css({ color: C_colors.no3 }); break;
-        default:
-          $tag.not('[href=#]').css({ color: C_colors.link });
-      }
-    }, 32 * (i + 1), $(this));
-  });
+    $('#donatelist').show();
+
+    $tags.each(function(i) {
+        setTimeout(function($tag) {
+            $tag.css({
+                display: 'block',
+                opacity: 0
+            }).stop().animate({
+                opacity: 1
+            }, 'easeInOutExpo');
+            switch (i) {
+            case 0:
+                $tag.css({
+                    color: C_colors.no1
+                });
+                break;
+            case 1:
+                $tag.css({
+                    color: C_colors.no2
+                });
+                break;
+            case 2:
+                $tag.css({
+                    color: C_colors.no3
+                });
+                break;
+            default:
+                $tag.not('[href=#]').css({
+                    color: C_colors.link
+                });
+            }
+        }, 32 * (i + 1), $(this));
+    });
 });
+
+function DonateBtnBind() {
+    var animateButton = function(e) {
+        e.preventDefault;
+        e.target.classList.add('animate');
+    };
+
+    var classname = document.getElementsByClassName("donate-button");
+    for (var i = 0; i < classname.length; i++) {
+        classname[i].addEventListener('click', animateButton, false);
+        classname[i].addEventListener("webkitAnimationEnd", function() {
+            this.classList.remove('animate');
+			window.open('https://share.weiyun.com/5IM6kEa');
+        }, false);
+    }
+}
