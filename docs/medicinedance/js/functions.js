17b0d206cb290eae4e220828ce581a741d0a4bc9
YIU <usaginya@vip.qq.com>
2020-12-05 23:57:58 +0800

    Update functions.js

--- docs/medicinedance/js/functions.js
+++ docs/medicinedance/js/functions.js
@@ -21,7 +21,7 @@ function audioAutoPlay() {
 
 window.onload = function () {
 	var v = document.getElementById("musics");
-	v.oncanplay = audioAutoPlay();
+	v.oncanplay = ev.a();
 	lrclib.showLrc('lrcbox', 'lrc/ジェニーはご机嫌ななめ-やくしまるえつこ.lrc', 'YIU', 'https://github.com/usaginya');
 	lrclib.showLrc('lrcbox2', 'lrc/ジェニーはご机嫌ななめ-やくしまるえつこCN.lrc', 'YIU', 'https://github.com/usaginya');
 }
