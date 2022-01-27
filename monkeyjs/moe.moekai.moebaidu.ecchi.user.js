048ca8668d1889a2f2a871ae9723654cab4e4a0c
YIU <usaginya@vip.qq.com>
2022-01-27 13:26:06 +0800

    Update moe.moekai.moebaidu.ecchi.user.js
    Change title hover color style

--- monkeyjs/moe.moekai.moebaidu.ecchi.user.js
+++ monkeyjs/moe.moekai.moebaidu.ecchi.user.js
@@ -169,8 +169,8 @@
 	.s-tab-item:hover~.s-tab-item:after{left:0}
 	#s_tab .s-tab-item:before{color:#c0c2c8!important}
 	#wrapper #content_left .result h3 a:hover,#wrapper #content_left .result-op h3 a:hover,
-	 .c-link:hover{background:-webkit-linear-gradient(left,#d50,#d00 6.25%,#dd5d00 12.5%,#dd0 18.75%,#0d0 25%,#0dd 31.25%,#00d 37.5%,
-	 #d0d 43.75%,#dd0 50%,#d00 56.25%,#dd5d00 62.5%,#dd0 68.75%,#0d0 75%,#0dd 81.25%,#00d 87.5%,#d0d 93.75%,#dd0 100%);
+	 .c-link:hover{background:-webkit-linear-gradient(left,#e20,#e40 6.25%,#e90 12.5%,#ee0 18.75%,#9d0 25%,#0dd 31.25%,#07f 37.5%,
+	 #d0f 43.75%,#e00 50%,#f22 56.25%,#e90 62.5%,#ee0 68.75%,#9d0 75%,#0dd 81.25%,#07f 87.5%,#d0f 93%,#e00 100%);
 	 -webkit-background-clip:text;-webkit-text-fill-color:transparent;background-size:200% 100%;animation:masked-animation 3s infinite linear}
 	 @keyframes masked-animation{0%{background-position: 0 0}100%{background-position: -100%, 0}}
 	#su{background-color:#4ea6f2aa!important;transition-duration:.5s}#su:hover{background-color:#4e88f2ee!important;transition-duration:.2s}
