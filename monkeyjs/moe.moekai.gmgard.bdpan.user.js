f1a41ccb5364aca55d969a4773e44963968d1af0
YIU <usaginya@vip.qq.com>
2022-04-12 22:59:09 +0800

    Update moe.moekai.gmgard.bdpan.user.js

--- monkeyjs/moe.moekai.gmgard.bdpan.user.js
+++ monkeyjs/moe.moekai.gmgard.bdpan.user.js
@@ -1,23 +1,25 @@
 // ==UserScript==
 // @name         绅士の庭度盘链接提取码补全
 // @namespace    moe.moekai.gmgard.bdpan
-// @version      1.5
+// @version      1.6
 // @description  将提取码补到度盘链接后面,配合网盘自动填写密码脚本使用
 // @author       YIU
-// @match        *://gmgard.com/gm*
-// @icon         http://gmgard.com/favicon.ico
+// @match        http*://gmgard.com/*
+// @match        http*://hggard.com/*
+// @match        http*://gmgard.moe/*
+// @icon         https://hggard.com/favicon.ico
 // @grant        unsafeWindow
 // ==/UserScript==
 
 (function() {
-	var $ = unsafeWindow.jQuery;
-	var codesv = 0;
+	let $ = unsafeWindow.jQuery;
+	let codesv = 0;
 
 	//取提取码
 	function getcode(){
-		var getval;
-		var reg = /^[^\x00-\xff][a-z\d]$|^[a-z\d][^\x00-\xff]$|^[a-z\d]{4}$/g;
-		var codes = [];
+		let getval;
+		let reg = /^\s*[a-z\d]{4}\s*$|^\s*[^\x00-\xff]{1}[a-z\d]{2}\s*$|^\s*[a-z\d]{2}[^\x00-\xff]{1}\s*$|^\s*[a-z\d]{1}[^\x00-\xff]{1}[a-z\d]{1}\s*$/gi;
+		let codes = [];
 		$('.label-inverse').each(function(){
 			getval = $(this).text();
 			if(getval.match(reg)) codesv = codes.push(getval);
@@ -26,20 +28,20 @@
 	}
 
 
-	var code = getcode();
+	let code = getcode();
 	if(code !== undefined && codesv > 0)
 	{
 		$('#dllist a').mouseenter();
 
-		var i = 0;
+		let i = 0;
 		$('#dllist a').each(function(){
-			var panurl = $(this)[0].href.indexOf("n.baidu");
+			let panurl = $(this)[0].href.indexOf("n.baidu");
 			if(panurl > 0)
 			{
 				i = i<codesv ? i : i-1;
 				$(this).attr("href",$(this)[0].href + '#' + code[i]);
 				i++;
-				o = this;
+				//o = this;
 			}
 		});
 	}
