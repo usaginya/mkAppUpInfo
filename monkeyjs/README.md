44a9106a93990cf15b3144a4a4c6c5a85bb2d32f
YIU <usaginya@vip.qq.com>
2022-05-13 02:47:46 +0800

    update gm script

--- monkeyjs/README.md
+++ monkeyjs/README.md
@@ -6,8 +6,8 @@
 | moe.moekai.exhentai-tooltip.user.js  | ExHentai缩略图列表标题悬浮提示  |
 | moe.moekai.gmgard.bdpan.user.js  | 绅士の庭度盘链接提取码补全(配合网盘自动填写)  |
 | moe.moekai.gmgardtransparentpost.user.js  | 绅士之庭半透明界面  |
-| plus.yasuotu.com.user.js  | 压缩图-功能提升  |
 | moe.moekai.tiebaindexpure.user.js  | 百度贴吧首页精简修改  |
+| moekai.moe.cleartiebaad.user.js  | 清理百度贴吧插入广告  |
 | moe.moekai.moebaidu.user.js | 百度娘搜索萌化 |
 | moe.moekai.moebaidu.ecchi.user.js | 百度娘搜索萌化（18+限制级） |
 | moe.moekai.aya.videorecorder.user.js | 文文录像机-网页视频录像 |
---
+++ monkeyjs/moekai.moe.cleartiebaad.user.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name         清理百度贴吧插入广告
+// @icon         http://www.gzqiyi.cn/fximg/delicious.gif
+// @namespace    moekai.moe.cleartiebaad
+// @version      1.2.0
+// @description  清理掉贴吧列表和贴子中插入的广告
+// @author       YIU
+// @match        *://tieba.baidu.com/f*
+// @match        *://tieba.baidu.com/p/*
+// @run-at       document-start
+// @require      https://libs.baidu.com/jquery/2.0.3/jquery.min.js
+// @compatible   chrome OK
+// @compatible   firefox OK
+// ==/UserScript==
+
+//======== 你可以修改这下面的开关数值 (0不清理 1清理) ========
+//清理楼层右下角成就图标
+var delams = 1;
+
+
+
+//======== 清理样式构建,不要修改 =========//
+var kamikakushi = '<style>';
+
+//----------- 神隐AD -----------//
+
+//-- 贴吧贴子列表, 贴子中插入
+kamikakushi += '.threadlist_bright>li:not([data-tid]), [ad-dom-img], .fengchao-wrap-feed, [class*=-ad-], .tbui_aside_float_bar + div';
+
+//-- 贴子楼层成就图标
+if(delams > 0){
+	kamikakushi += ',.achievement_medal_section';
+}
+
+//-- 贴吧背景左右链接, 右边栏推荐应用
+kamikakushi += ',body>.j_couplet, .aside_region.my_app';
+
+//-- 贴子顶部banner, 右边栏图片
+kamikakushi += ',#banner_pb_customize, #branding_ads, #aside-ad, #fc-wrap';
+//kamikakushi += ',#banner_pb_customize, #branding_ads';
+
+//-- 贴吧底部关注提示
+kamikakushi += ',.xiu8_follow_warn';
+
+//------! 神隐 !------
+kamikakushi += '{display:none!important}';
+
+
+//---- 神隐保护 (以ID选择来提高优先级) ----//
+
+//-- 置顶贴列表
+kamikakushi += '#thread_list>.thread_top_list_folder';
+
+//------! 保护 !------
+kamikakushi += '{display:block!important}';
+
+//-----------------------------------//
+kamikakushi += '</style>';
+
+//======== 清理样式构建结束 =========//
+
+
+//======== 执行区,不要修改 =======
+(function(){
+
+	$("head").append(kamikakushi);
+
+})();
