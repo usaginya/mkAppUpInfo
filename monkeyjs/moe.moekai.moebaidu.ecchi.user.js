40db43b1a35d2d16c2a7cbb3061999ef099d2df4
YIU <usaginya@vip.qq.com>
2024-01-21 22:23:01 +0800

    Update moe.moekai.moebaidu.ecchi.user.js
    Change translation card style

--- monkeyjs/moe.moekai.moebaidu.ecchi.user.js
+++ monkeyjs/moe.moekai.moebaidu.ecchi.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         度娘搜索萌化ecchi
 // @namespace    https://cdn.jsdelivr.net/gh/usaginya/mkAppUpInfo@master/monkeyjs/moe.moekai.moebaidu.ecchi.user.js
-// @version      3.9.2
+// @version      3.9.3
 // @description  萌化度娘搜索R18限制级 [18+]
 // @author       YIU
 // @icon         https://www.baidu.com/favicon.ico
@@ -284,9 +284,13 @@
 	.x-interact-publish .emoj-panel{margin:0 12px;width:96%;right:0!important}
 	.xdp .no-margin-left{margin-right:10px}
 	#wrapper div[class*=img-content-wrap_],.darkmode.dark #wrapper div[class*=head-wrap-new_]{background:#fff0!important}
-	#wrapper div[class*=content-wrapper_],#wrapper div[class*=weather-top_]{background-image:none!important;background-color:#fff0!important}
+	#wrapper div[class*=content-wrapper_],#wrapper div[class*=content-wrapper_] [class*=content-box_],
+	 #wrapper div[class*=weather-top_]{background-image:none!important;background-color:#fff0!important}
 	#wrapper div[class*=forecast_]{background:none!important}
 	#wrapper div[class*=weather-top_] div[class*=bg-wrapper_]{margin-top:-10px;opacity:0.7}
+	.result-op[tpl*="fanyi"] [class*=horizontal-gradient_]{border:none!important}
+	.result-op[tpl*="fanyi"] [class*=operate-box_] [class*=audio-box_],
+	 .result-op[tpl*="fanyi"] [class*=operate-wrapper-box_] [class*=operate-wrapper]{background:#fff6!important}
 	[class*=chart-wrapper_] [class*=tip-alert_]{margin-right:22px}
 	.darkmode.dark [class*=chart-wrapper_] [class*=tip-alert_]{background-color:#222;color:#eee;}
 	.darkmode.dark [class*=-chart-wrapper_] [class*=price-text_],
@@ -480,7 +484,7 @@
 	 +`:not([class*=op_weather]):not([class*=full-star_]):not([class*=card-more-icon_]):not([class*=unselected_] i):not([class^=right-icon_])`
 	 +`:not([class^=source-icon_]):not([class*=live-icon]):not([class*=arrow-icon]):not([class*=theme-icon]):not([class*=avatar])`
 	 +`:not(.c-showurl [class*=icon_]):not([class*=weather-icon_]):not([class*=icon-gap-]):not([class*=qihou-icon_]):not([class*=state-icon_])`
-	 +`:not([class*=pause]):not([class*=play]):not([class*=audio-wrapper_]),
+	 +`:not([class*=pause]):not([class*=play]):not([class*=audio-wrapper_]):not([class*=change-icon_]),
 	 .darkmode.dark #wrapper [class*=wrap_] [class*=opt-emoji_],.darkmode.dark #wrapper .c-icon [class*=img2_],
 	 .darkmode.dark #wrapper .c-icon[class*=wrapper_],.darkmode.dark #wrapper [class*=select-icon],
 	 .darkmode.dark #wrapper [class*=icon-more_],.darkmode.dark #wrapper [class*=title-icon-row_],
